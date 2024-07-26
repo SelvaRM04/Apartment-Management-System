224aa1b7614a008449985d2f18dc26d1149d8cb2
Selva Rathinam  M <selva152004@gmail.com>
2024-07-26 23:06:54 +0530

    Final Changes

--- db/migrate/20240611210232_drop_p_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class DropPToAdmin < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :admins, :password, :password_digest
-  end
-end
---
+++ db/migrate/20240726171308_add_password_digest_to_admin.rb
@@ -0,0 +1,5 @@
+class AddPasswordDigestToAdmin < ActiveRecord::Migration[7.1]
+  def change
+    add_column :admins, :password_digest, :string
+  end
+end
